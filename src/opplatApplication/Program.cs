9c4221cccfd11bc30527fae998730c3827cfa31b
Elvis Jr Crego <elvis.crego@gmail.com>
2019-09-03 13:08:57 -0400

    cambio para cargar dinammico los demas proyectos

--- src/opplatApplication/Program.cs
+++ src/opplatApplication/Program.cs
@@ -8,26 +8,102 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System.Net;
-
+using opplatApplication.Utils;
+using System.Reflection;
+using System.Runtime.Loader;
 
 namespace opplatApplication
 {
     public class Program
     {
+        private static IList<ModuleInfo> _modules = new List<ModuleInfo>();
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseKestrel(options =>  
-            {  
-                // var config = new ConfigurationBuilder()  
-                //     .AddJsonFile($"appsettings.{hostingEnvironment.EnvironmentName}.json", optional: false)  
-                //     .Build();  
-                //options.Listen(IPAddress.Loopback, config.GetValue<int>("Host:Port"));  
-                options.Listen(IPAddress.Loopback, 5200);  
-            }) .UseStartup<Startup>();
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            //LoadInstalledModules();
+            var host = WebHost.CreateDefaultBuilder(args)
+               .UseKestrel(options =>
+           {
+               options.Listen(IPAddress.Loopback, 5200);
+           })
+           .UseSetting(WebHostDefaults.HostingStartupAssembliesKey, "opplatApplication;Account.WebApi");
+            return host;
+        }
+
+        // public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+
+        //     WebHost.CreateDefaultBuilder(args)
+        //         .UseKestrel(options =>
+        //     {
+        //         options.Listen(IPAddress.Loopback, 5200);
+        //     })
+        //     .UseSetting(WebHostDefaults.HostingStartupAssembliesKey, "opplatApplication;Account.WebApi");
+        //.UseStartup<Startup>();
+
+        public static void LoadInstalledModules()
+        {
+            //var section = _configuration.GetSection("Modules");
+            var _configuration = new ConfigurationBuilder().AddJsonFile("./appsettings.json").Build();
+            var modulesPath = _configuration.GetValue<string>("Modules:ModulesPath");
+            var moduleRootFolder = new DirectoryInfo("/media/elvis/datos/proyectos/netcore/opplat/src/");
+            //            var moduleRootFolder = new DirectoryInfo(Path.Combine(_hostingEnvironment.ContentRootPath, modulesPath));
+            var moduleFolders = moduleRootFolder.GetDirectories();
+
+            foreach (var moduleFolder in moduleFolders)
+            {
+                var file_name = moduleFolder.Name + ".dll";
+                var directorios = moduleFolder.EnumerateFiles(file_name, SearchOption.AllDirectories).ToList();
+                var dllFile = directorios.FirstOrDefault(d => d.Name == file_name);
+                if (dllFile == null)
+                {
+                    continue;
+                }
+                var binFolder = dllFile.Directory;
+                //var binFolder = new DirectoryInfo(Path.Combine(moduleFolder.FullName, "bin"));
+                if (binFolder == null || !binFolder.Exists)
+                {
+                    continue;
+                }
+
+                foreach (var file in binFolder.GetFileSystemInfos("*.dll", SearchOption.TopDirectoryOnly))
+                {
+                    Assembly assembly = null;
+                    try
+                    {
+                        assembly = AssemblyLoadContext.Default.LoadFromAssemblyPath(file.FullName);
+                    }
+                    catch (FileLoadException ex)
+                    {
+                        if (ex.Message == "Assembly with same name is already loaded")
+                        {
+                            // Get loaded assembly
+                            assembly = Assembly.Load(new AssemblyName(Path.GetFileNameWithoutExtension(file.Name)));
+                        }
+                        else
+                        {
+                            throw;
+                        }
+                    }
+
+                    if (assembly.FullName.Contains(moduleFolder.Name))
+                    {
+                        _modules.Add(new ModuleInfo { Name = moduleFolder.Name, Assembly = assembly, Path = moduleFolder.FullName });
+                    }
+                    else
+                    {
+                        if (!GlobalConfiguration.OtherAssemblies.Contains(assembly))
+                        {
+                            GlobalConfiguration.OtherAssemblies.Add(assembly);
+                        }
+                    }
+                }
+            }
+
+            GlobalConfiguration.Modules = _modules;
+        }
     }
 }
--- src/opplatApplication/Startup.cs
+++ src/opplatApplication/Startup.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -17,34 +20,60 @@ namespace opplatApplication
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private IConfiguration _configuration { get; }
+        private IHostingEnvironment _hostingEnvironment { get; }
+        private readonly IList<ModuleInfo> _modules = new List<ModuleInfo>();
+        public Startup(IConfiguration configuration, IHostingEnvironment environment)
         {
-            Configuration = configuration;
+            _configuration = configuration;
+            _hostingEnvironment = environment;
         }
 
-        public IConfiguration Configuration { get; }
+
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // services.AddCors(c =>
-            // {
-            //     c.AddPolicy("AllowAnyOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().AllowCredentials());
-            // });
-            // services.Configure<CookiePolicyOptions>(options =>
-            // {
-            //     // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-            //     options.CheckConsentNeeded = context => true;
-            //     options.MinimumSameSitePolicy = SameSiteMode.None;
-            // });
+            //LoadInstalledModules();
+
             services.AddDbContext<OpplatAppDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("opplatApplication")));
-            services.AddTransient<DbContext, OpplatAppDbContext>();
+                options.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("opplatApplication")));
+            services.AddTransient<OpplatAppDbContext>();
 
             services.AddSingleton<MenuLoader>();
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            var mvcBuilder = services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             // var loader = new MicroserviceLoader();
             // loader.AddMicroservice("/");
+            //var moduleInitializerInterface = typeof(IModuleInitializer);
+            foreach (var module in _modules)
+            {
+                // Register controller from modules
+                //mvcBuilder.AddApplicationPart(module.Assembly);
+
+                // Register dependency in modules
+                // var moduleInitializerType = module.Assembly.GetTypes().Where(x => typeof(IHostingStartup).IsAssignableFrom(x)).FirstOrDefault();
+                // if (moduleInitializerType != null && moduleInitializerType != typeof(IHostingStartup))
+                // {
+                //     var moduleInitializer = (IHostingStartup)Activator.CreateInstance(moduleInitializerType);                    
+                // }
+            }
+
+            // TODO: break down to new method in new class
+            //var builder = new ContainerBuilder();
+            // foreach (var module in GlobalConfiguration.Modules)
+            // {
+            //     builder.RegisterAssemblyTypes(module.Assembly).AsImplementedInterfaces();
+            // }
+            // foreach (var assembly in GlobalConfiguration.OtherAssemblies)
+            // {
+            //     builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces();
+            // }
+
+            // builder.RegisterInstance(Configuration);
+            // builder.RegisterInstance(_hostingEnvironment);
+            // builder.Populate(services);
+            // var container = builder.Build();
+            // return container.Resolve<IServiceProvider>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -65,8 +94,68 @@ namespace opplatApplication
             app.UseStaticFiles();
             app.UseCors(build => build.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod().AllowCredentials());
             //app.UseCookiePolicy();
-
+            //app.Map("account", account => { account.UseMvc(); });
             app.UseMvc();
+        }
+
+        private void LoadInstalledModules()
+        {
+            //var section = _configuration.GetSection("Modules");
+            var modulesPath = _configuration.GetValue<string>("Modules:ModulesPath");
+            var moduleRootFolder = new DirectoryInfo(Path.Combine(_hostingEnvironment.ContentRootPath, modulesPath));
+            var moduleFolders = moduleRootFolder.GetDirectories();
+
+            foreach (var moduleFolder in moduleFolders)
+            {
+                var file_name = moduleFolder.Name + ".dll";
+                var directorios = moduleFolder.EnumerateFiles(file_name, SearchOption.AllDirectories).ToList();
+                var dllFile = directorios.FirstOrDefault(d => d.Name == file_name);
+                if (dllFile == null)
+                {
+                    continue;
+                }
+                var binFolder = dllFile.Directory;
+                //var binFolder = new DirectoryInfo(Path.Combine(moduleFolder.FullName, "bin"));
+                if (binFolder == null || !binFolder.Exists)
+                {
+                    continue;
+                }
+
+                foreach (var file in binFolder.GetFileSystemInfos("*.dll", SearchOption.TopDirectoryOnly))
+                {
+                    Assembly assembly = null;
+                    try
+                    {
+                        assembly = AssemblyLoadContext.Default.LoadFromAssemblyPath(file.FullName);
+                    }
+                    catch (FileLoadException ex)
+                    {
+                        if (ex.Message == "Assembly with same name is already loaded")
+                        {
+                            // Get loaded assembly
+                            assembly = Assembly.Load(new AssemblyName(Path.GetFileNameWithoutExtension(file.Name)));
+                        }
+                        else
+                        {
+                            throw;
+                        }
+                    }
+
+                    if (assembly.FullName.Contains(moduleFolder.Name))
+                    {
+                        _modules.Add(new ModuleInfo { Name = moduleFolder.Name, Assembly = assembly, Path = moduleFolder.FullName });
+                    }
+                    else
+                    {
+                        if (!GlobalConfiguration.OtherAssemblies.Contains(assembly))
+                        {
+                            GlobalConfiguration.OtherAssemblies.Add(assembly);
+                        }
+                    }
+                }
+            }
+
+            GlobalConfiguration.Modules = _modules;
         }
     }
 }
