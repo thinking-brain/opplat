df1f237ea46964d8daaffe11cb6f6de0f1dde32f
Elvis Jr Crego <elvis.crego@gmail.com>
2019-09-24 09:08:10 -0400

    agregando configurador y cambios de rutas

--- src/FinanzasWebApi/Controllers/GastosIndirectosController.cs
+++ src/FinanzasWebApi/Controllers/GastosIndirectosController.cs
@@ -6,26 +6,35 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using FinanzasWebApi.ViewModels;
 using System.Net.Http;
+using Microsoft.Extensions.Configuration;
 
 namespace FinanzasWebApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("finanzas/[controller]")]
     [ApiController]
     public class GastosIndirectosController : ControllerBase
     {
+        IConfiguration _config { get; set; }
+
+        public GastosIndirectosController(IConfiguration config)
+        {
+            _config = config;
+        }
+
         /// <summary>
         /// Reporte de Gastos Indirectos
         /// </summary>
         /// <param name="años"></param>
         /// <param name="mes"></param>
         /// <returns></returns>
-        [HttpGet("api/GastosIndirectos/{años}/{mes}")]
+        [HttpGet("finanzas/GastosIndirectos/{años}/{mes}")]
         public List<GastosIndirectosVM> GastosIndirectos([FromRoute] string años, int mes)
         {
             //Periodo
             HttpClient clientPeriodo = new HttpClient();
             List<GenPeriodoVM> periodo = new List<GenPeriodoVM>();
-            var resultPeriodo = clientPeriodo.GetAsync( "http://localhost:5050/api/GenPeriodo").Result;
+            var url = _config.GetValue<string>("FinanzasApi") + "/GenPeriodo";
+            var resultPeriodo = clientPeriodo.GetAsync(url).Result;
             if (resultPeriodo.IsSuccessStatusCode)
             {
                 periodo = resultPeriodo.Content.ReadAsAsync<List<GenPeriodoVM>>().Result;
@@ -34,28 +43,30 @@ namespace FinanzasWebApi.Controllers
             //OptCuentaCentroSubPeriodo
             HttpClient clientOpt = new HttpClient();
             List<OptCuentaCentroSubPeriodoVM> opt = new List<OptCuentaCentroSubPeriodoVM>();
-            var resultOpt = clientOpt.GetAsync( "http://localhost:5050/api/OptCuentaCentroSubPeriodo").Result;
+            var url2 = _config.GetValue<string>("FinanzasApi") + "/OptCuentaCentroSubPeriodo";
+            var resultOpt = clientOpt.GetAsync(url2).Result;
             if (resultOpt.IsSuccessStatusCode)
             {
                 opt = resultOpt.Content.ReadAsAsync<List<OptCuentaCentroSubPeriodoVM>>().Result;
             }
-          
-           //CosSubelementogasto
+
+            //CosSubelementogasto
             HttpClient clientSubElemento = new HttpClient();
             List<CosSubelementogastoVM> subElemento = new List<CosSubelementogastoVM>();
-            var resultSubElemento = clientSubElemento.GetAsync( "http://localhost:5050/api/CosSubelementogastos").Result;
+            var urlSubelement = _config.GetValue<string>("FinanzasApi") + "/CosSubelementogastos";
+            var resultSubElemento = clientSubElemento.GetAsync(urlSubelement).Result;
             if (resultSubElemento.IsSuccessStatusCode)
             {
                 subElemento = resultSubElemento.Content.ReadAsAsync<List<CosSubelementogastoVM>>().Result;
             }
-          
+
 
             int year;
             year = Convert.ToInt32(años);
             var data = new List<GastosIndirectosVM>();
             var periodoInicio = periodo.SingleOrDefault(s => s.Inicio.Year == year && s.Inicio.Month == 1);
             var periodoFin = periodo.SingleOrDefault(s => s.Inicio.Year == year && s.Inicio.Month == mes);
-            foreach (var item in opt.Where(s=>s.Idperiodo >= periodoInicio.Idperiodo && s.Idperiodo <= periodoFin.Idperiodo && s.Clavecuenta.Equals("731")).ToList().GroupBy(s=>s.Idsub))
+            foreach (var item in opt.Where(s => s.Idperiodo >= periodoInicio.Idperiodo && s.Idperiodo <= periodoFin.Idperiodo && s.Clavecuenta.Equals("731")).ToList().GroupBy(s => s.Idsub))
             {
                 var sub = subElemento.SingleOrDefault(s => s.Idsubelemento == item.Key);
                 data.Add(new GastosIndirectosVM { DescripcionSubElemento = sub.Descripcion, Valor = item.Sum(s => s.Debito) });
