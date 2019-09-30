00ba28fa829e555ae4c557b8d671378b8b9f1094
Elvis Jr Crego <elvis.crego@gmail.com>
2019-09-30 15:12:43 -0400

    agregando proyecto para importar datos

---
+++ src/ImportadorDatos/Startup.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Hangfire.PostgreSql;
+using Hangfire;
+
+namespace ImportadorDatos
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHangfire(config =>
+                config.UsePostgreSqlStorage(Configuration.GetConnectionString("HangfireConnection")));
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+            //Fire-and-Forget
+BackgroundJob.Enqueue(() => Console.WriteLine("Fire-and-forget"));
+//Delayed
+BackgroundJob.Schedule(() => Console.WriteLine("Delayed"), TimeSpan.FromDays(1));
+//Recurring
+RecurringJob.AddOrUpdate(() => Console.WriteLine("Minutely Job"), Cron.Minutely);
+//Continuation
+var id = BackgroundJob.Enqueue(() => Console.WriteLine("Hello, "));
+BackgroundJob.ContinueWith(id, () => Console.WriteLine("world!"));
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
