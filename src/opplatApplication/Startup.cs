80f3e093ecdd5c73a2397151cc74fff331c538ac
VAlmea <almeavicente@gmail.com>
2019-09-30 15:01:51 -0700

    arreglando el string de coneccion en finanzas api

--- src/opplatApplication/Startup.cs
+++ src/opplatApplication/Startup.cs
@@ -48,7 +48,7 @@ namespace opplatApplication
             services.AddDbContext<VersatDbContext2>(options =>
                options.UseSqlServer(context.Configuration.GetConnectionString("Versat2Connection")));
             //
- 
+
             //finanzas db context
             services.AddDbContext<FinanzasWebApi.Data.ApiDbContext>(options =>
                     options.UseNpgsql(context.Configuration.GetConnectionString("FinanzasApiDbContext"), b => b.MigrationsAssembly("FinanzasWebApi")));
