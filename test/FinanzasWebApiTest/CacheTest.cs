728cef59af4d11e3935f0529ec6114d5b8766e29
Elvis Jr Crego <elvis.crego@gmail.com>
2019-10-31 08:21:20 -0400

    Agregando proyecto de pruebas de Finanzas

---
+++ test/FinanzasWebApiTest/CacheTest.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace FinanzasWebApiTest
+{
+    public class CacheTest
+    {
+        [Fact]
+        public void UpdateCacheTest()
+        {
+
+        }
+
+        [Fact]
+        public void CacheCuentaTest()
+        {
+
+        }
+    }
+}
