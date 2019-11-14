39a6d304175fe04abeb3bdeb1c8eb5864433b6cd
Ariel <ariel.blanco1990@gmail.com>
2019-11-14 16:18:27 -0500

    Razones Financieras

---
+++ src/FinanzasWebApi/Controllers/RazonesFinancierasController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using CsvHelper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FinanzasWebApi.Data;
+using FinanzasWebApi.Helper;
+using FinanzasWebApi.ViewModels;
+using Microsoft.Extensions.Configuration;
+
+namespace FinanzasWebApi.Controllers
+{
+    [Route("finanzas/[controller]")]
+    [ApiController]
+    public class RazonesFinancierasController : ControllerBase
+    {
+        ObtenerValuesEnVariablesEstadoFinanciero _obtenerVariables { get; set; }
+        ObtenerPlanGI _obtenetPlan { get; set; }
+        IConfiguration _config { get; set; }
+
+        public RazonesFinancierasController(ObtenerValuesEnVariablesEstadoFinanciero obtenerVariables, ObtenerPlanGI obtenerPlan, IConfiguration config)
+        {
+            _config = config;
+            _obtenerVariables = obtenerVariables;
+            _obtenetPlan = obtenerPlan;
+        }
+
+
+
+
+        /// <summary>
+        /// Devuelve el valor de la Razon (Solvencia Financiera)
+        /// </summary>
+        /// <param name="años"></param>
+        /// <param name="meses"></param>
+        /// <returns></returns>
+        [HttpGet("solvenciaFinanciera/{años}/{meses}")]
+        public decimal solvenciaFinanciera(int años, int meses)
+        {
+            decimal ingresos = _obtenetPlan.ObtenerTotalIngresos(años, meses);
+            decimal egresos = _obtenetPlan.ObtenerTotalEgresos(años, meses);
+            decimal resultado = egresos != 0 ? ingresos / egresos : 0;
+            return Math.Round(resultado, 2);
+        }
+
+        /// <summary>
+        /// Devuelve el valor de la Razon (Capital  de trabajo)
+        /// </summary>
+        /// <param name="años"></param>
+        /// <param name="meses"></param>
+        /// <returns></returns>
+        [HttpGet("capitalDeTrabajo/{años}/{meses}")]
+        public decimal capitalDeTrabajo(int años, int meses)
+        {
+            decimal ac = _obtenerVariables.activoCirculante(años, meses);
+            decimal pc = _obtenerVariables.pasivoCirculante(años, meses);
+            decimal resultado = ac - pc;
+            return resultado;
+        }
+
+        /// <summary>
+        /// Devuelve el valor de la Razon (Indice de liquidez Genaral)
+        /// </summary>
+        /// <param name="años"></param>
+        /// <param name="meses"></param>
+        /// <returns></returns>
+        [HttpGet("indiceDeLiquidezGneral/{años}/{meses}")]
+        public decimal indiceDeLiquidezGneral(int años, int meses)
+        {
+            decimal ac = _obtenerVariables.activoCirculante(años, meses);
+            decimal pc = _obtenerVariables.pasivoCirculante(años, meses);
+            decimal resultado = pc != 0 ? ac / pc : 0;
+            return resultado;
+        }
+
+        /// <summary>
+        /// Devuelve el valor de la Razon (Liquidez de Tesorería)
+        /// </summary>
+        /// <param name="años"></param>
+        /// <param name="meses"></param>
+        /// <returns></returns>
+        [HttpGet("liquidezDeTesoreria/{años}/{meses}")]
+        public decimal liquidezDeTesoreria(int años, int meses)
+        {
+            //ACTIVOS LIQUIDOS NO ESTAN
+            decimal al = _obtenerVariables.activoCirculante(años, meses);
+            decimal pc = _obtenerVariables.pasivoCirculante(años, meses);
+            decimal resultado = pc != 0 ? al / pc : 0;
+            return resultado;
+        }
+
+
+        /// <summary>
+        /// Devuelve el valor de la Razon (Indice de deuda o Razon de Endeudamiento)
+        /// </summary>
+        /// <param name="años"></param>
+        /// <param name="meses"></param>
+        /// <returns></returns>
+        [HttpGet("indiceDeDeudaORazonDeEndeudamiento/{años}/{meses}")]
+        public decimal indiceDeDeudaORazonDeEndeudamiento(int años, int meses)
+        {
+            decimal pc = _obtenerVariables.pasivoCirculante(años, meses);
+            decimal plp = _obtenerVariables.pasivosALargoPlazo(años, meses);
+            decimal pd = _obtenerVariables.pasivosDiferidos(años, meses);
+            decimal op = _obtenerVariables.otrosPasivos(años, meses);
+            decimal tp = pc + plp + pd + op;
+
+            decimal ac = _obtenerVariables.activoCirculante(años, meses);
+            decimal ad = _obtenerVariables.activosDiferidos(años, meses);
+            decimal af = _obtenerVariables.activosFijos(años, meses);
+            decimal alp = _obtenerVariables.activosLargoPlazo(años, meses);
+            decimal oa = _obtenerVariables.otrosActivos(años, meses);
+            decimal ta = ac + ad + af + alp + oa;
+
+            decimal resultado = ta != 0 ? tp / ta : 0;
+            return resultado;
+        }
+
+
+        /// <summary>
+        /// Devuelve el valor de la Razon (Indice de disponibilidad)
+        /// </summary>
+        /// <param name="años"></param>
+        /// <param name="meses"></param>
+        /// <returns></returns>
+        [HttpGet("indiceDeDisponibilidad/{años}/{meses}")]
+        public decimal indiceDeDisponibilidad(int años, int meses)
+        {
+            decimal ec = _obtenerVariables.efectivoEnCaja(años, meses);
+            decimal eb = _obtenerVariables.efectivoEnBanco(años, meses);
+            decimal ac = _obtenerVariables.activoCirculante(años, meses);
+
+            decimal resultado = ac != 0 ? (ec + eb) / ac : 0;
+            return resultado;
+        }
+
+
+
+    }
+}
--- src/FinanzasWebApi/Controllers/VariablesFinancierasController.cs
+++ src/FinanzasWebApi/Controllers/VariablesFinancierasController.cs
@@ -39,61 +39,8 @@ namespace FinanzasWebApi.Controllers
         [HttpGet("totalInventario/{años}/{meses}")]
         public decimal VariableTI(int años, int meses)
         {
-            decimal materiasPrimasYMateriales = _obtenerVariables.Valor("Materias Primas y Materiales", años, meses);
-            decimal combustiblesYLubricantes = _obtenerVariables.Valor("Combustibles y Lubricantes", años, meses);
-            decimal partesYPiezasDeRespuesto = _obtenerVariables.Valor("Partes y Piezas de Respuesto", años, meses);
-            decimal envasesYEmbalajes = _obtenerVariables.Valor("Envases y Embalajes", años, meses);
-            decimal utilesHerramientasYOtros = _obtenerVariables.Valor("Utiles, Herramientas y Otros", años, meses);
-            decimal produccionTerminada = _obtenerVariables.Valor("Producción Terminada", años, meses);
-            decimal mercanciasParaLaVenta = _obtenerVariables.Valor("Mercancias para la Venta", años, meses);
-            decimal medicamentos = _obtenerVariables.Valor("Medicamentos", años, meses);
-            decimal baseMaterialDeEstudio = _obtenerVariables.Valor("Base Material de Estudio", años, meses);
-            decimal vestuarioYLenceria = _obtenerVariables.Valor("Vestuario y Lencería", años, meses);
-            decimal alimentos = _obtenerVariables.Valor("Alimentos", años, meses);
-            decimal inventarioDeMercanciasDeImportacion = _obtenerVariables.Valor("Inventario de Mercancías de Importación", años, meses);
-            decimal inventarioDeMercanciasDeExportación = _obtenerVariables.Valor("Inventario de Mercancias de Exportación", años, meses);
-            decimal produccionesParaInsumoOAutoconsumo = _obtenerVariables.Valor("Producciones para Insumo o Autoconsumo", años, meses);
-            decimal otrosInventarios = _obtenerVariables.Valor("Otros Inventarios", años, meses);
-            decimal inventariosOciosos = _obtenerVariables.Valor("Inventarios Ociosos", años, meses);
-            decimal inventariosDeLentoMovimiento = _obtenerVariables.Valor("Inventarios de lento Movimiento", años, meses);
-            decimal produccionEnProceso = _obtenerVariables.Valor("Producción en Proceso", años, meses);
-            decimal produccionPropiaParaInsumoOAutoconsumo = _obtenerVariables.Valor("Produccion Propia para Insumo o Autoconsumo", años, meses);
-            decimal reparacionesCapitalesConMediosPropios = _obtenerVariables.Valor("Reparaciones Capitales con Medios Propios", años, meses);
-            decimal inversionesConMediosPropios = _obtenerVariables.Valor("Inversiones con Medios Propios", años, meses);
-
-
-            decimal desgasteDeUtilesYHerramientas = _obtenerVariables.Valor("Desgaste de Utiles y Herramientas", años, meses);
-            decimal menosDesgasteDeBaseMaterialDeEstudio = _obtenerVariables.Valor("Menos: Desgaste de Base Material de Estudio", años, meses);
-            decimal menosDesgasteDeVestuarioYLenceria = _obtenerVariables.Valor("Menos: Desgaste de Vestuario y Lenceria", años, meses);
-
-
-            decimal resultado =
-              (
-              materiasPrimasYMateriales
-            + combustiblesYLubricantes
-            + partesYPiezasDeRespuesto
-            + envasesYEmbalajes
-            + utilesHerramientasYOtros
-            + produccionTerminada
-            + mercanciasParaLaVenta
-            + medicamentos
-            + baseMaterialDeEstudio
-            + vestuarioYLenceria
-            + alimentos
-            + inventarioDeMercanciasDeImportacion
-            + inventarioDeMercanciasDeExportación
-            + produccionesParaInsumoOAutoconsumo
-            + otrosInventarios
-            + inventariosOciosos
-            + inventariosDeLentoMovimiento
-            + produccionEnProceso
-            + produccionPropiaParaInsumoOAutoconsumo
-            + reparacionesCapitalesConMediosPropios
-            + inversionesConMediosPropios
-            )
-            - (desgasteDeUtilesYHerramientas + menosDesgasteDeBaseMaterialDeEstudio - menosDesgasteDeVestuarioYLenceria)
-            ;
-            return resultado;
+            decimal valor = _obtenerVariables.InventarioTotal(años, meses);
+            return valor;
         }
 
 
@@ -106,33 +53,9 @@ namespace FinanzasWebApi.Controllers
         [HttpGet("activoCirculante/{años}/{meses}")]
         public decimal VariableAC(int años, int meses)
         {
-            decimal cuentaEnParticipacion = _obtenerVariables.Valor("Cuenta en Participación", años, meses);
-            decimal cuentasPorCobrarACortoPlazo = _obtenerVariables.Valor("Cuentas por Cobrar a Corto Plazo", años, meses);
-            decimal efectivoEnCaja = _obtenerVariables.Valor("Efectivo en Caja", años, meses);
-            decimal efectivoEnBancoYEnOtrasInstituciones = _obtenerVariables.Valor("Efectivo en Banco y en otras instituciones", años, meses);
-            decimal inversionesACortoPlazoOTemporales = _obtenerVariables.Valor("Inversiones a Corto Plazo o Temporales", años, meses);
-            decimal efectosPorCobrarACortoPlazo = _obtenerVariables.Valor("Efectos por Cobrar a Corto Plazo", años, meses);
-            decimal pagosPorCuentasDeTerceros = _obtenerVariables.Valor("Pagos por Cuentas de Terceros", años, meses);
-            decimal creditosDocumentarios = _obtenerVariables.Valor("Créditos Documentarios", años, meses);
-            decimal totalInventario = _obtenerVariables.InventarioTotal(años, meses);
-
-            decimal efectosPorCobrarDescontados = _obtenerVariables.Valor("Efectos por Cobrar Descontados", años, meses);
-            decimal provisionParaCuentasIncobrables = _obtenerVariables.Valor("Provisión para Cuentas Incobrables", años, meses);
-
-            decimal resultado =
-              (cuentaEnParticipacion
-            + cuentasPorCobrarACortoPlazo
-            + efectivoEnCaja
-            + efectivoEnBancoYEnOtrasInstituciones
-            + inversionesACortoPlazoOTemporales
-            + efectosPorCobrarACortoPlazo
-            + pagosPorCuentasDeTerceros
-            + creditosDocumentarios
-            + totalInventario) - (efectosPorCobrarDescontados - provisionParaCuentasIncobrables)
-            ;
-            return resultado;
+            decimal valor = _obtenerVariables.activoCirculante(años, meses);
+            return valor;
         }
-
 
         /// <summary>
         /// Devuelve los Activos a Largo Plazo
@@ -143,18 +66,8 @@ namespace FinanzasWebApi.Controllers
         [HttpGet("activosLargoPlazo/{años}/{meses}")]
         public decimal VariableALP(int años, int meses)
         {
-            decimal efectosPorCobrarALargoPlazo = _obtenerVariables.Valor("Efectos por Cobrar a Largo Plazo", años, meses);
-            decimal cuentasPorCobrarACortoPlazo = _obtenerVariables.Valor("Cuentas por Cobrar a Largo Plazo", años, meses);
-            decimal prestamosConcedidosACobrarALargoPlazo = _obtenerVariables.Valor("Préstamos Concedidos a Cobrar a Largo Plazo", años, meses);
-            decimal inversionesALargoPlazoOPermanentes = _obtenerVariables.Valor("Inversiones a Largo Plazo o Permanentes", años, meses);
-
-            decimal resultado =
-              efectosPorCobrarALargoPlazo
-            + cuentasPorCobrarACortoPlazo
-            + prestamosConcedidosACobrarALargoPlazo
-            + inversionesALargoPlazoOPermanentes
-            ;
-            return resultado;
+            decimal valor = _obtenerVariables.activosLargoPlazo(años, meses);
+            return valor;
         }
 
         /// <summary>
@@ -166,29 +79,9 @@ namespace FinanzasWebApi.Controllers
         [HttpGet("activosFijos/{años}/{meses}")]
         public decimal VariableAFT(int años, int meses)
         {
-            decimal mediosYEquiposParaAlquilar = _obtenerVariables.Valor("Medios y Equipos para Alquilar", años, meses);
-            decimal fondosBibliotecarios = _obtenerVariables.Valor("Fondos Bibliotecarios", años, meses);
-            decimal aft = _obtenerVariables.Valor("Activos Fijos Tangibles", años, meses);
-            decimal monumentosYObrasDeArte = _obtenerVariables.Valor("Monumentos y Obras de Arte", años, meses);
-            decimal afi = _obtenerVariables.Valor("Activos Fijos Intangibles", años, meses);
-            decimal inversionesEnProceso = _obtenerVariables.Valor("Inversiones en Proceso", años, meses);
-            decimal equiposPorInstalarMaterialesParaElProcesoInversionista = _obtenerVariables.Valor("Equipos por Instalar y Materiales para el Proceso Inversionista", años, meses);
+            decimal valor = _obtenerVariables.activosFijos(años, meses);
+            return valor;
 
-            decimal daft = _obtenerVariables.Valor("Depreciación de Activos Fijos Tangibles", años, meses);
-            decimal desgasteDeMediosYEquiposParaAlquilar = _obtenerVariables.Valor("Desgaste de Medios y Equipos para Alquilar", años, meses);
-            decimal amortizacionDeActivosFijosIntangibles = _obtenerVariables.Valor("Amortización de Activos Fijos Intangibles", años, meses);
-
-            decimal resultado =
-              (mediosYEquiposParaAlquilar
-            + fondosBibliotecarios
-            + aft
-            + monumentosYObrasDeArte
-            + afi
-            + inversionesEnProceso
-            + equiposPorInstalarMaterialesParaElProcesoInversionista)
-            - (daft - desgasteDeMediosYEquiposParaAlquilar - amortizacionDeActivosFijosIntangibles)
-            ;
-            return resultado;
         }
 
         /// <summary>
@@ -200,16 +93,9 @@ namespace FinanzasWebApi.Controllers
         [HttpGet("activosDiferidos/{años}/{meses}")]
         public decimal VariableAFD(int años, int meses)
         {
-            decimal gastosDeProduccionServiciosDiferidos = _obtenerVariables.Valor("Gastos de Produccion y Servicios Diferidos", años, meses);
-            decimal gastosFinancierosDiferidos = _obtenerVariables.Valor("Gastos Financieros Diferidos", años, meses);
-            decimal gastosDiferidosDelProcesoInversionista = _obtenerVariables.Valor("Gastos Diferidos del Proceso Inversionista", años, meses);
+            decimal valor = _obtenerVariables.activosDiferidos(años, meses);
+            return valor;
 
-            decimal resultado =
-              gastosDeProduccionServiciosDiferidos
-            + gastosFinancierosDiferidos
-            + gastosDiferidosDelProcesoInversionista
-            ;
-            return resultado;
         }
 
         /// <summary>
@@ -221,12 +107,132 @@ namespace FinanzasWebApi.Controllers
         [HttpGet("otrosActivos/{años}/{meses}")]
         public decimal VariableOA(int años, int meses)
         {
-            decimal perdidasEnInvestigacion = _obtenerVariables.Valor("Pérdidas en Investigación", años, meses);
+            decimal valor = _obtenerVariables.otrosActivos(años, meses);
+            return valor;
+        }
 
-            decimal resultado =
-              perdidasEnInvestigacion
-            ;
-            return resultado;
+
+        /// <summary>
+        /// Devuelve el valor del Total de Activos
+        /// </summary>
+        /// <param name="años"></param>
+        /// <param name="meses"></param>
+        /// <returns></returns>
+        [HttpGet("totalDeActivos/{años}/{meses}")]
+        public decimal VariableTA(int años, int meses)
+        {
+            decimal ac = _obtenerVariables.activoCirculante(años, meses);
+            decimal alp = _obtenerVariables.activosLargoPlazo(años, meses);
+            decimal af = _obtenerVariables.activosFijos(años, meses);
+            decimal ad = _obtenerVariables.activosDiferidos(años, meses);
+            decimal oa = _obtenerVariables.otrosActivos(años, meses);
+            decimal valor = ac + alp + af + ad + oa;
+            return valor;
+        }
+
+        /// <summary>
+        /// Devuelve el valor los Pasivos Circulantes
+        /// </summary>
+        /// <param name="años"></param>
+        /// <param name="meses"></param>
+        /// <returns></returns>
+        [HttpGet("pasivosCirculantes/{años}/{meses}")]
+        public decimal VariablePC(int años, int meses)
+        {
+            decimal valor = _obtenerVariables.pasivoCirculante(años, meses);
+            return valor;
+        }
+
+        /// <summary>
+        /// Devuelve el valor los Pasivos A Largo Plazo
+        /// </summary>
+        /// <param name="años"></param>
+        /// <param name="meses"></param>
+        /// <returns></returns>
+        [HttpGet("pasivosLargoPlazo/{años}/{meses}")]
+        public decimal VariablePLP(int años, int meses)
+        {
+            decimal valor = _obtenerVariables.pasivosALargoPlazo(años, meses);
+            return valor;
+        }
+        /// <summary>
+        /// Devuelve el valor los Pasivos Diferidos
+        /// </summary>
+        /// <param name="años"></param>
+        /// <param name="meses"></param>
+        /// <returns></returns>
+        [HttpGet("pasivosDiferidos/{años}/{meses}")]
+        public decimal VariablePD(int años, int meses)
+        {
+            decimal valor = _obtenerVariables.pasivosDiferidos(años, meses);
+            return valor;
+        }
+        /// <summary>
+        /// Devuelve el valor los Otros Pasivos
+        /// </summary>
+        /// <param name="años"></param>
+        /// <param name="meses"></param>
+        /// <returns></returns>
+        [HttpGet("otrosPasivos/{años}/{meses}")]
+        public decimal VariableOP(int años, int meses)
+        {
+            decimal valor = _obtenerVariables.otrosPasivos(años, meses);
+            return valor;
+        }
+
+        /// <summary>
+        /// Devuelve el valor los Totales de Pasivos
+        /// </summary>
+        /// <param name="años"></param>
+        /// <param name="meses"></param>
+        /// <returns></returns>
+        [HttpGet("totalPasivos/{años}/{meses}")]
+        public decimal VariableTP(int años, int meses)
+        {
+            decimal pc = _obtenerVariables.pasivoCirculante(años, meses);
+            decimal plp = _obtenerVariables.pasivosALargoPlazo(años, meses);
+            decimal pd = _obtenerVariables.pasivosDiferidos(años, meses);
+            decimal op = _obtenerVariables.otrosPasivos(años, meses);
+            decimal valor = pc + plp + pd + op;
+            return valor;
+        }
+        /// <summary>
+        /// Devuelve el valor de Efectivo en Caja
+        /// </summary>
+        /// <param name="años"></param>
+        /// <param name="meses"></param>
+        /// <returns></returns>
+        [HttpGet("efectivoEnCaja/{años}/{meses}")]
+        public decimal VariableEC(int años, int meses)
+        {
+            decimal valor = _obtenerVariables.efectivoEnCaja(años, meses);
+            return valor;
+        }
+
+        /// <summary>
+        /// Devuelve el valor de Efectivo en Banco
+        /// </summary>
+        /// <param name="años"></param>
+        /// <param name="meses"></param>
+        /// <returns></returns>
+        [HttpGet("efectivoEnBanco/{años}/{meses}")]
+        public decimal VariableEB(int años, int meses)
+        {
+            decimal valor = _obtenerVariables.efectivoEnBanco(años, meses);
+            return valor;
+        }
+
+        /// <summary>
+        /// Devuelve el valor del Patrimonio Neto Total
+        /// </summary>
+        /// <param name="años"></param>
+        /// <param name="meses"></param>
+        /// <returns></returns>
+        [HttpGet("totalPatrimonioNeto/{años}/{meses}")]
+        public decimal VariableTPN(int años, int meses)
+        {
+            decimal valor = _obtenerVariables.totalDePatrimonioNeto(años, meses);
+            return valor;
         }
 
     }
