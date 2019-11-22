492c840faf2f0e6947b2a2e42875009c8a9d39ae
Elvis Jr Crego <elvis.crego@gmail.com>
2019-11-21 23:46:51 -0500

    Agregando proyecto inicial de ContratacionWebApi

---
+++ src/ContratacionWebApi/Startup.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+using System.Reflection;
+using System.IO;
+
+namespace ContratacionWebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddSwaggerGen(c =>
+               {
+                   c.SwaggerDoc("contratacion_v1", new Info
+                   {
+                       Version = "v1",
+                       Title = "Contratacion API",
+                       Description = "Gestión de contratacion dentro del Sistema OPPLAT.",
+                       TermsOfService = "APACHE 2.0",
+                       Contact = new Contact
+                       {
+                           Name = "EFAVAI Tech",
+                           Email = "efavai.tech@gmail.com",
+                           Url = "https://efavai.com/"
+                       }
+                   });
+
+                   // Set the comments path for the Swagger JSON and UI.
+                   var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                   var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                   c.IncludeXmlComments(xmlPath);
+               });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseSwagger(c => c.RouteTemplate = "docs/{documentName}/docs.json");
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/docs/contratacion_v1/docs.json", "Contratacion API v1");
+                c.RoutePrefix = "docs";
+            });
+            app.UseMvc();
+        }
+    }
+}
