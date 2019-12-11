802023b0e7dd1fcc4ccebe2f35c2c9f75b369cfc
Ariel <ariel.blanco1990@gmail.com>
2019-12-11 09:31:14 -0500

    Estados Financieros VMs

---
+++ src/FinanzasWebApi/ViewModels/DetalleEstadoFinancieroVM.cs
@@ -0,0 +1,8 @@
+namespace FinanzasWebApi.ViewModels
+{
+    public class DetalleEstadoFinancieroVM
+    {
+        public string Razon { get; set; }
+        public decimal Valor { get; set; }
+    }
+}
---
+++ src/FinanzasWebApi/ViewModels/EstadoFinancieroVM.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace FinanzasWebApi.ViewModels
+{
+    public class EstadoFinancieroVM
+    {
+        public string Concepto { get; set; }
+        public string EFE { get; set; }
+        public List<DetalleEstadoFinancieroVM> Detalles { get; set; }
+
+
+    }
+}
