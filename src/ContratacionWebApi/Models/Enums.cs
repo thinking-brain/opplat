d786da3b5044c780cf5438c14af108cc2a8be18f
Elvis Jr Crego <elvis.crego@gmail.com>
2020-01-21 14:49:24 -0500

    contratacion

---
+++ src/ContratacionWebApi/Models/Enums.cs
@@ -0,0 +1,23 @@
+namespace ContratacionWebApi.Models
+{
+    public enum Tipo
+    {
+        Marco,
+        Compra,
+        Venta,
+        Servicio,
+    }
+
+    public enum Estado
+    {
+        Nuevo,
+        Circulando,
+        Aprobado,
+        NoAprobado,
+        Vigente,
+        Cancelado,
+        Vencido,
+        Revision,
+        SinEstado
+    }
+}
