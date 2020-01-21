b9fcc1a64a6c85541bc5c0b3fa6175700ee67d67
Elvis Jr Crego <elvis.crego@gmail.com>
2020-01-21 14:49:04 -0500

    cambios rh no se

---
+++ src/RhWebApi/Migrations/20191115014430_plantillaOcupada.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RhWebApi.Migrations
+{
+    public partial class plantillaOcupada : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CantidadPorPlantilla",
+                table: "puestos_de_trabajos",
+                newName: "PlantillaOcupada");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PlantillaOcupada",
+                table: "puestos_de_trabajos",
+                newName: "CantidadPorPlantilla");
+        }
+    }
+}
