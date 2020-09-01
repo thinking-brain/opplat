478a91e6f8c4c014673fb1622265fd70b849d0fb
VAlmea <almeavicente@gmail.com>
2020-09-01 13:48:28 -0700

    creando servicio para enviar correos

--- src/opplatApplication/Controllers/MenusController.cs
+++ src/opplatApplication/Controllers/MenusController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using opplatApplication.Models;
+using opplatApplication.Services;
 using opplatApplication.Utils;
 
 namespace opplatApplication.Controllers
