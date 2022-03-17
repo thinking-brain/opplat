199f5e3029603eb489b09dc986c74b67155c9658
Elvis Jr Crego <elvis.crego@gmail.com>
2022-03-17 11:26:15 -0400

    dependencias de proyectos y roles

---
+++ src/Opplat.MainApp/Utils/ErpRoles.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Opplat.MainApp.Utils;
+
+public class ErpRoles
+{
+    public static const string Administrador = "ADMINISTRADOR";
+    public static const string Almacen = "ALMACEN";
+    public static const string Cajera = "CAJERA";
+}
